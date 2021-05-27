5497d2c70a2aae7210ae47950376b9925edf9b27
AnnieJuly <76923850+AnnieJuly@users.noreply.github.com>
2021-05-27 19:35:02 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,43 @@
+var Engine = Matter.Engine,
+  World = Matter.World,
+  Events = Matter.Events,
+  Bodies = Matter.Bodies;
+
+ 
+
+  var particles = [];
+  var plinkos = [];
+  var divisions =[];
+  var divisionHeight=300;
+  var score =0;
+
+function setup() {
+   createCanvas(800,400);
+  engine = Engine.create();
+  world = engine.world;
+  ground = new Ground(width/2,height,width,20);
+
+  for (var k = 0; k <=width; k = k + 80) {
+     divisions.push(new Divisions(k, height-divisionHeight/2, 10, divisionHeight));
+   }
+
+   for (var j = 75; j <=width; j=j+50) 
+   {
+   
+      plinkos.push(new Plinko(j,75));
+   }
+
+}
+
+function draw() {
+  background("black");
+  textSize(20)
+
+  Engine.update(engine);
+  ground.display();
+  for (var i = 0; i < plinkos.length; i++) {
+     
+    plinkos[i].display();
+    
+  }
+}
